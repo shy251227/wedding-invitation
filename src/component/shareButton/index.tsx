c8b7881723d4ff46540a95f461beba5d4276cacc
shy251227 <ckzz0213@gmail.com>
2025-09-21 22:49:03 +0900

    25.09.21 kakao 공유1

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -24,43 +24,23 @@ export const ShareButton = () => {
           }
 
           kakao.Share.sendDefault({
-            objectType: "location",
-            address: SHARE_ADDRESS,
-            addressTitle: SHARE_ADDRESS_TITLE,
+            objectType: "feed",
             content: {
               title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME}의 결혼식에 초대합니다.`,
-              description:
-                WEDDING_DATE.format("YYYY년 MMMM D일 dddd A h시") +
-                "\n" +
-                LOCATION,
-              imageUrl:
-                window.location.protocol +
-                "//" +
-                window.location.host +
-                baseUrl +
-                "/preview_image.png",
+              //년 월 일
+              description:'25년 12월 27일 토요일 오전 11시 W시티 컨벤션 울산 웨딩홀',
+              //실제 청첩장 이미지
+              imageUrl:'https://github.com/shy251227/wedding-invitation/blob/main/src/images/image1.png',
               link: {
-                mobileWebUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  baseUrl,
-                webUrl:
-                  window.location.protocol +
-                  "//" +
-                  window.location.host +
-                  baseUrl,
+                mobileWebUrl:'https://shy251227.github.io/wedding-invitation/',
+                webUrl:'https://shy251227.github.io/wedding-invitation/',
               },
             },
             buttons: [
               {
                 title: "초대장 보기",
                 link: {
-                  mobileWebUrl:
-                    window.location.protocol +
-                    "//" +
-                    window.location.host +
-                    baseUrl,
+                  mobileWebUrl: 'https://shy251227.github.io/wedding-invitation/',
                   webUrl:
                     window.location.protocol +
                     "//" +
@@ -77,3 +57,77 @@ export const ShareButton = () => {
     </LazyDiv>
   )
 }
+/*import { useEffect } from "react"
+// ERROR FIX: 빌드 오류를 해결하기 위해 파일 경로를 상대 경로로 수정했습니다.
+import ktalkIcon from "../../icons/ktalk-icon.png"
+import "./index.scss"
+
+// TypeScript가 window.Kakao를 인식하도록 설정합니다.
+declare global {
+  interface Window {
+    Kakao: any
+  }
+}
+
+export const ShareButton = () => {
+  const currentUrl = window.location.href
+
+  // 컴포넌트가 처음 로딩될 때 카카오 SDK를 초기화합니다.
+  useEffect(() => {
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init("7d71cfdbf0064e046226776f19adfde2")
+    }
+  }, [])
+
+  const handleCopyClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(currentUrl)
+      alert("링크가 복사되었습니다. 원하는 곳에 붙여넣기 하세요.")
+    } catch (e) {
+      alert("복사에 실패하였습니다")
+    }
+  }
+
+  // 카카오톡 공유 버튼을 눌렀을 때 실행될 함수
+  const handleShareKakao = () => {
+    if (window.Kakao) {
+      window.Kakao.Share.sendDefault({
+        objectType: "feed",
+        content: {
+          title: "정하윤 💕 차승훈 결혼합니다.",
+          description: "25년 12월 27일 토요일 오전 11시 W시티 컨벤션 울산 웨딩홀",
+          imageUrl:
+            "https://raw.githubusercontent.com/shy251227/wedding-invitation/main/src/images/cover.png",
+          link: {
+            mobileWebUrl: "https://shy251227.github.io/wedding-invitation/",
+            webUrl: "https://shy251227.github.io/wedding-invitation/",
+          },
+        },
+        buttons: [
+          {
+            title: "청첩장 보러가기",
+            link: {
+              mobileWebUrl: "https://shy251227.github.io/wedding-invitation/",
+              webUrl: "https://shy251227.github.io/wedding-invitation/",
+            },
+          },
+        ],
+      })
+    }
+  }
+
+  return (
+    <div className="share-button-container">
+      <div className="text">공유하기</div>
+      <div className="button-wrapper">
+        {/* 카카오톡 공유 버튼에 onClick 이벤트 연결 }
+        <div className="button" onClick={handleShareKakao}>
+          <img src={ktalkIcon} alt="카카오톡으로 공유하기" />
+        </div>
+        <div className="button" onClick={handleCopyClipboard}>
+          <i className="fa-solid fa-link" />
+        </div>
+      </div>
+    </div>
+  )
+}*/
--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,4 @@
-// src/index.tsx
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ModalProvider } from "./component/modal";
-import { StoreProvider } from "./component/store";
-// ❗ 필수: 카카오 SDK 초기화 함수를 불러옵니다.
-import { initializeKakao } from "./kakao";
-// ❗ 필수: .env 파일에서 카카오 SDK 키를 불러옵니다.
-import { KAKAO_SDK_JS_KEY } from "./env";
-
-// ❗ 카카오 SDK 키가 존재하면 initializeKakao 함수를 호출하여 SDK를 초기화합니다.
-if (KAKAO_SDK_JS_KEY) {
-  initializeKakao(KAKAO_SDK_JS_KEY);
-} else {
-  console.warn("VITE_KAKAO_SDK_JS_KEY 환경 변수가 설정되지 않았습니다. 카카오 공유 기능이 비활성화됩니다.");
-}
-
-// index.html 파일에서 'root' ID를 가진 DOM 요소를 찾아 React 앱의 루트로 설정합니다.
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-
-// React 앱을 렌더링합니다.
-root.render(
-  // 개발 중 잠재적인 문제를 감지하기 위한 React StrictMode를 사용합니다.
-  <React.StrictMode>
-    {/* StoreProvider는 앱 전체의 전역 상태(예: SDK 로딩 여부)를 관리합니다. */}
-    <StoreProvider>
-      {/* ModalProvider는 앱 전체에서 사용될 모달(팝업) 기능을 제공합니다. */}
-      <ModalProvider>
-        {/* App 컴포넌트는 청첩장의 모든 내용을 담고 있는 최상위 컴포넌트입니다. */}
-        <App />
-      </ModalProvider>
-    </StoreProvider>
-  </React.StrictMode>
-);
-
-/*import React from "react"
+import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import { ModalProvider } from "./component/modal"
@@ -51,4 +14,3 @@ root.render(
     </ModalProvider>
   </React.StrictMode>,
 )
-*/
